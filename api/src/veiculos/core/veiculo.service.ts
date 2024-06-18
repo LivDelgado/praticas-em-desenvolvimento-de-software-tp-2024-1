f9c9570ecbb74830bc5bc50ee7a980a6b38d8de4
Lívia Delgado <23265664+LivDelgado@users.noreply.github.com>
2024-06-18 20:18:25 -0300

    feat: add api to get vehicle

--- api/src/veiculos/core/veiculo.service.ts
+++ api/src/veiculos/core/veiculo.service.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { VeiculosDataSource } from '../adapters/database/veiculo.datasource';
-import { VeiculoDto } from '../presentation/veiculo.dto';
+import { GetVeiculoDto, VeiculoDto } from '../presentation/veiculo.dto';
 
 @Injectable()
 export class VeiculosService {
@@ -17,22 +17,27 @@ export class VeiculosService {
     );
 
     if (veiculo) {
-      return VeiculoDto.fromVeiculo(veiculo);
+      return GetVeiculoDto.fromVeiculo(veiculo);
     }
 
     return null;
   }
 
-  async create(veiculo: VeiculoDto): Promise<VeiculoDto> {
+  async create(veiculo: VeiculoDto): Promise<GetVeiculoDto> {
     const veiculoCriado = await this.veiculosDataSource.save(
       VeiculoDto.toDomain(veiculo),
     );
 
-    return VeiculoDto.fromVeiculo(veiculoCriado);
+    return GetVeiculoDto.fromVeiculo(veiculoCriado);
   }
 
-  async list(): Promise<VeiculoDto[]> {
+  async list(): Promise<GetVeiculoDto[]> {
     const veiculos = await this.veiculosDataSource.findAll();
-    return veiculos.map((it) => VeiculoDto.fromVeiculo(it));
+    return veiculos.map((it) => GetVeiculoDto.fromVeiculo(it));
+  }
+
+  async getById(id: number): Promise<GetVeiculoDto> {
+    const veiculo = await this.veiculosDataSource.findById(id);
+    return GetVeiculoDto.fromVeiculo(veiculo);
   }
 }
--- api/src/veiculos/presentation/veiculo.controller.ts
+++ api/src/veiculos/presentation/veiculo.controller.ts
@@ -1,7 +1,15 @@
-import { Body, Controller, Get, Post, UseFilters } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseFilters,
+} from '@nestjs/common';
 import { HttpExceptionFilter } from '../../http-exception.filter';
 import { VeiculosService } from '../core/veiculo.service';
-import { VeiculoDto } from './veiculo.dto';
+import { GetVeiculoDto, VeiculoDto } from './veiculo.dto';
 
 @Controller('veiculos')
 @UseFilters(new HttpExceptionFilter())
@@ -9,13 +17,21 @@ export class VeiculoController {
   constructor(private veiculoService: VeiculosService) {}
 
   @Post()
-  async post(@Body() veiculo: VeiculoDto): Promise<VeiculoDto> {
+  async post(@Body() veiculo: VeiculoDto): Promise<GetVeiculoDto> {
     return this.veiculoService.create(veiculo);
   }
 
   @Get()
   @UseFilters(new HttpExceptionFilter())
-  async get(): Promise<VeiculoDto[]> {
+  async list(): Promise<GetVeiculoDto[]> {
     return this.veiculoService.list();
+  }
+
+  @Get('/:veiculoId')
+  @UseFilters(new HttpExceptionFilter())
+  async get(
+    @Param('veiculoId', new ParseIntPipe()) veiculoId,
+  ): Promise<GetVeiculoDto> {
+    return this.veiculoService.getById(veiculoId);
   }
 }
