b9fbc51615ee52e9252b9906e9706f9656fc334d
Gabriel Chaves <42660456+gabrielchaves7@users.noreply.github.com>
2024-06-22 12:19:42 -0300

    VeiculosTable style

--- frotas-app/src/veiculos/components/tabelaVeiculos.tsx
+++ frotas-app/src/veiculos/components/tabelaVeiculos.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { VeiculoApi } from '../api/veiculo.api';
 import { Veiculo } from '../types/veiculo';
 import { useNavigate } from 'react-router-dom';
+import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, styled, tableCellClasses } from '@mui/material';
 
 function TabelaVeiculos() {
   const [veiculos, setVeiculos] = useState<Veiculo[]>([]);
@@ -37,37 +38,74 @@ function TabelaVeiculos() {
       } catch (error) {
         console.error(error);
       }
-      }
+    }
   };
 
+
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: 'white',
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
+
+  const tableCellStyles = {
+    backgroundColor: '#f5f5f5',
+    color: '#a4a4a4',
+  };
+
+
+  const PaddingButton = styled(Button)(({ theme }) => ({
+    marginRight: '8px'
+  }));
+
+
   return (
+
     <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Nome</th>
-            <th>Data de aquisição</th>
-            {/* <th>Motorista</th> */}
-            <th>Próxima manutenção</th>
-            <th>Ações</th>
-          </tr>
-        </thead>
-        <tbody>
-          {veiculos.map((veiculo) => (
-            <tr key={veiculo.id}>
-              <td>{veiculo.montadora + veiculo.modelo}</td>
-              <td>{new Date(veiculo.dataAquisicao).toISOString().slice(0, 10)}</td>
-              {/* <td>{veiculo.driver}</td> */}
-              <td>{veiculo.nextManutencaoDate ? new Date(veiculo.nextManutencaoDate).toISOString().slice(0, 10) : null}</td>
-              <td>
-              <button onClick={() => handleManutencao(veiculo)}>Manutenção</button>
-              <button onClick={() => handleEdit(veiculo)}>Editar</button>
-                <button onClick={() => handleRemove(veiculo)}>Remover</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 700 }} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <TableCell sx={tableCellStyles}>Nome</TableCell>
+              <TableCell sx={tableCellStyles} align="center">Data de aquisição</TableCell>
+              <TableCell sx={tableCellStyles} align="center">Próxima manutenção</TableCell>
+              <TableCell sx={tableCellStyles} align="center"></TableCell>
+              {/* <TableCell align="right">Ações</TableCell> */}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {veiculos.map((veiculo) => (
+              <StyledTableRow key={veiculo.id}>
+                <TableCell component="th" scope="row">
+                  {veiculo.montadora + veiculo.modelo}
+                </TableCell>
+                <TableCell align="center">{new Date(veiculo.dataAquisicao).toISOString().slice(0, 10)}</TableCell>
+                <TableCell align="center">{veiculo.nextManutencaoDate ? new Date(veiculo.nextManutencaoDate).toISOString().slice(0, 10) : null}</TableCell>
+                <TableCell align="right">
+                  <PaddingButton onClick={() => handleManutencao(veiculo)} variant="outlined" sx={{
+                    color: '#FFCD00',
+                    borderColor: '#FFCD00',
+                    padding: 'right: 8'
+                  }}>Manutenção</PaddingButton>
+
+                  <PaddingButton onClick={() => handleRemove(veiculo)} variant="outlined" sx={{
+                    color: '#FE0404',
+                    borderColor: '#FE0404',
+                  }}>Remover</PaddingButton>
+                  <PaddingButton onClick={() => handleEdit(veiculo)} variant="outlined" sx={{
+                    color: '#1554F6',
+                    borderColor: '#1554F6',
+                  }}>Editar</PaddingButton>
+                </TableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
