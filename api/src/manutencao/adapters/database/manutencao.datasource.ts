867d129104db1c0e9416466e983a04e28b8c8230
Lívia Delgado <23265664+LivDelgado@users.noreply.github.com>
2024-06-23 17:51:42 -0300

    refactor: rename repository field

--- api/src/manutencao/adapters/database/manutencao.datasource.ts
+++ api/src/manutencao/adapters/database/manutencao.datasource.ts
@@ -10,31 +10,31 @@ import { Manutencao } from 'src/manutencao/core/manutencao';
 export class ManutencaoDataSource implements IManutencaoRepository {
   constructor(
     @InjectRepository(ManutencaoEntity)
-    private readonly ManutencaoRepository: Repository<ManutencaoEntity>,
+    private readonly manutencaoRepository: Repository<ManutencaoEntity>,
   ) {}
 
   async save(manutencao: Manutencao): Promise<Manutencao> {
-    const newManutencao = this.ManutencaoRepository.create(
+    const newManutencao = this.manutencaoRepository.create(
       ManutencaoEntity.fromDomain(manutencao),
     );
-    const created = await this.ManutencaoRepository.save(newManutencao);
+    const created = await this.manutencaoRepository.save(newManutencao);
     return created.toDomain();
   }
 
   async update(id: number, manutencao: Manutencao): Promise<Manutencao> {
     manutencao.id = id;
-    const updated = await this.ManutencaoRepository.save(
+    const updated = await this.manutencaoRepository.save(
       ManutencaoEntity.fromDomain(manutencao),
     );
     return updated.toDomain();
   }
 
   async deleteById(id: number): Promise<void> {
-    this.ManutencaoRepository.delete({ id: id });
+    this.manutencaoRepository.delete({ id: id });
   }
 
   async findByVeiculoId(veiculoId: number): Promise<Manutencao[]> {
-    const manutencoes = await this.ManutencaoRepository.find({
+    const manutencoes = await this.manutencaoRepository.find({
       where: { veiculo: { id: veiculoId } },
     });
 
