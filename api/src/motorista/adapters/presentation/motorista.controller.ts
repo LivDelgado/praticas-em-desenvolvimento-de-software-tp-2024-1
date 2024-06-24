15ae2481e322218cf42a00173140ec94fa648f43
Eduardo Cotta <dudufrancamg@gmail.com>
2024-06-23 21:39:45 -0300

    chore: add update motorista method

--- api/src/motorista/adapters/presentation/motorista.controller.ts
+++ api/src/motorista/adapters/presentation/motorista.controller.ts
@@ -3,14 +3,19 @@ import {
   Controller,
   HttpException,
   HttpStatus,
+  Param,
+  ParseIntPipe,
   Post,
+  Put,
   UseFilters,
 } from '@nestjs/common';
 import { HttpExceptionFilter } from 'src/http-exception.filter';
 import { GetMotoristaDto, MotoristaDto } from './motorista.dto';
 import { IMotoristaService } from 'src/motorista/core/ports/inbound/IMotoristaService';
+import { ApiParam, ApiTags } from '@nestjs/swagger';
 
 @Controller('motorista/')
+@ApiTags('Motoristas')
 @UseFilters(new HttpExceptionFilter())
 export class MotoristaController {
   constructor(private motoristaService: IMotoristaService) {}
@@ -26,5 +31,23 @@ export class MotoristaController {
     } catch (exception) {
       throw new HttpException(exception.message, HttpStatus.BAD_REQUEST);
     }
+  }
+
+  @Put('/:motoristaId')
+  @ApiParam({
+    name: 'motoristaId',
+    required: true,
+    description: 'ID do motorista',
+    type: Number,
+  })
+  async put(
+    @Param('motoristaId', new ParseIntPipe()) motoristaId: number,
+    @Body() motorista: MotoristaDto,
+  ): Promise<GetMotoristaDto> {
+    const updated = await this.motoristaService.update(
+      motoristaId,
+      MotoristaDto.toDomain(motorista),
+    );
+    return GetMotoristaDto.fromDomain(updated);
   }
 }
