606c38bbb4f255f5018c47173138e986be5e6b9b
Lívia Delgado <23265664+LivDelgado@users.noreply.github.com>
2024-06-24 14:57:08 -0300

    seed: insert veiculos

--- api/seeder/seed.sql
+++ api/seeder/seed.sql
@@ -1,8 +1,18 @@
 BEGIN TRANSACTION;
 
--- SEED
+INSERT INTO "veiculo" (
+    "montadora",
+    "modelo",
+    "ano",
+    "dataAquisicao"
+) VALUES
+    ('Hyundai', 'HB20', '2020', '2022-03-17T00:00:00.000Z'),
+    ('Toyota', 'Etios', '2021', '2022-03-18T00:00:00.000Z'),
+    ('Toyota', 'Corolla', '2021', '2022-03-18T00:00:00.000Z'),
+    ('Fiat', 'Punto', '2022', '2022-05-07T00:00:00.000Z'),
+    ('Volkswagen', 'Voyage', '2022', '2022-05-08T00:00:00.000Z'),
+    ('Volkswagen', 'Gol', '2023', '2024-01-02T00:00:00.000Z'),
+    ('Fiat', 'Uno', '2023', '2024-01-02T00:00:00.000Z'),
+    ('Honda', 'Civic', '2023', '2024-01-02T00:00:00.000Z');
 
 COMMIT;
-
-
-SELECT 'Hello, PostgreSQL!' AS message;
